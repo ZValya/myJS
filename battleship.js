a04b4f76a7eb7cb413b5f8a49ff6a8b619465150
ZValya <bashkirovs@rambler.ru>
2018-07-09 20:34:00 +0300

    vanya

--- battleship.js
+++ battleship.js
@@ -2,7 +2,7 @@ var view = {
     displayMessage: function(msg) {
         var messageArea = document.getElementById("messageArea");
         messageArea.innerHTML = msg;
-        msg = "привет";
+        
         
     },
     displayHit: function(location) {
@@ -16,10 +16,45 @@ var view = {
         cell.setAttribute("class", "miss");
 }
 };
-    view.displayMessage = ("да");
+    view.displayMessage("Tap tap, is this thing on?");
     view.displayMiss("00");
     view.displayHit("34");
     view.displayMiss("55");
     view.displayHit("12");
     view.displayMiss("25");
     view.displayHit("26");
+
+
+/*function validate(phoneNumber) {
+    if (phoneNumber.length !== 8) {
+        return false;
+    }
+    for (var i = 0; i < phoneNumber.length; i++) {
+        if (i === 3) {
+            if (phoneNumber.charAt(i) !== "-") {
+                return false;
+            }
+        } else if (isNaN(phoneNumber.charAt(i))) {
+            return false;
+        }
+    }
+    return true;
+}
+
+var number = validate("123-4567");
+console.log(number);*/
+
+/*function validate(phoneNumber) {
+    if (phoneNumber.length !== 8) {
+        return false;
+    }
+    var first = phoneNumber.substring(0,3);
+    var second = phoneNumber.substring(4);
+    if (phoneNumber.charAt(3) !=="-" || isNaN(first) || isNaN(second)) {
+        return false;
+    }
+    return true;
+}
+
+var number = validate("123-4567");
+console.log(number);*/
